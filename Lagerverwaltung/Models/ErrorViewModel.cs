d11fa621fdd1810161781fc75bf81aca66ed9367
Eric Müller <de.muller.eric@gmail.com>
2022-11-25 09:36:15 +0100

    Projektdateien hinzufügen.

---
+++ Lagerverwaltung/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lagerverwaltung.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
