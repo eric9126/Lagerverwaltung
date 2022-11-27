7af68606c673b75b2f7ceef84805a10b512d6e79
Eric Müller <de.muller.eric@gmail.com>
2022-11-27 21:23:17 +0100

    27.11.2022 2

---
+++ Lagerverwaltung/Controllers/AuftragController.cs
@@ -0,0 +1,72 @@
+﻿using Lagerverwaltung.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lagerverwaltung.Controllers
+{
+    public class AuftragController : Controller
+    {
+        private readonly Data.ApplicationDbContext _context;
+        public AuftragController(Data.ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var AuftragFromDB = _context.Auftrag.ToList();
+            return View(AuftragFromDB);
+        }
+
+        //Controller der Seite zum bearbeiten / hinzufügen
+        public IActionResult CreateEditAuftrag(int id)
+        {
+            if (id != 0)
+            {
+                var AuftragFromDB = _context.Auftrag.SingleOrDefault(x => x.Id == id);
+
+                if (AuftragFromDB != null)
+                {
+                    return View(AuftragFromDB);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return View();
+        }
+
+        //Controller der aufgerufen wird wenn der Speichern Button gedrückt wird
+        public IActionResult ExecuteCreateOrEditAuftrag(Models.Auftrag Auftrag)
+        {
+
+            if (Auftrag.Id == 0)
+            {
+
+                _context.Auftrag.Add(Auftrag);
+            }
+            else
+            {
+                var AuftragFromDB = _context.Auftrag.SingleOrDefault(x => x.Id == Auftrag.Id);
+
+                if (AuftragFromDB == null)
+                {
+                    return NotFound();
+                }
+
+                AuftragFromDB.KundeID = Auftrag.KundeID;
+                AuftragFromDB.Bemerkungen = Auftrag.Bemerkungen;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BackToAuftrag()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lagerverwaltung/Controllers/PositionController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lagerverwaltung.Controllers
+{
+    public class PositionController : Controller
+    {
+        private readonly Data.ApplicationDbContext _context;
+        public PositionController(Data.ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var PositionFromDB = _context.Position.ToList();
+            return View(PositionFromDB);
+        }
+
+        //Controller der Seite zum bearbeiten / hinzufügen
+        public IActionResult CreateEditPosition(int id)
+        {
+            if (id != 0)
+            {
+                var PositionFromDB = _context.Position.SingleOrDefault(x => x.Id == id);
+
+                if (PositionFromDB != null)
+                {
+                    return View(PositionFromDB);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return View();
+        }
+
+        //Controller der aufgerufen wird wenn der Speichern Button gedrückt wird
+        public IActionResult ExecuteCreateOrEditPosition(Models.Positionen Position)
+        {
+
+            if (Position.Id == 0)
+            {
+
+                _context.Position.Add(Position);
+            }
+            else
+            {
+                var PositionFromDB = _context.Position.SingleOrDefault(x => x.Id == Position.Id);
+
+                if (PositionFromDB == null)
+                {
+                    return NotFound();
+                }
+
+                PositionFromDB.AuftragsID = Position.AuftragsID;
+                PositionFromDB.PositionsNummer = Position.PositionsNummer;
+                PositionFromDB.ArtikelID = Position.ArtikelID;
+                PositionFromDB.Menge = Position.Menge;
+                PositionFromDB.Bemerkungen = Position.Bemerkungen;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BackToPosition()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
