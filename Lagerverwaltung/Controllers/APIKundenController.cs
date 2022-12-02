2959904088e5d7e878c0f5e9ed24d0dafef752c9
Eric Müller <de.muller.eric@gmail.com>
2022-12-02 14:39:47 +0100

    02.12.2022 Add APIKunden

---
+++ Lagerverwaltung/Controllers/APIKundenController.cs
@@ -0,0 +1,95 @@
+﻿using Lagerverwaltung.Data;
+using Lagerverwaltung.Filters;
+using Lagerverwaltung.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace Lagerverwaltung.Controllers
+{
+    [Route("api/Kunden")]
+    [ApiController]
+    [ApiKeyAuthorization]
+    public class APIKundenController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public APIKundenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var all = _context.Kunde.ToArray();
+            return Ok(all);
+        }
+
+        [HttpGet("GetById")] 
+        public IActionResult GetById(int id) 
+        { 
+        
+            var byId = _context.Kunde.SingleOrDefault(x => x.Id == id);
+
+            if(byId == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(byId);
+        
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(Kunde kunde)
+        {
+            if (kunde.Id != 0)
+            {
+                return BadRequest();
+            }
+
+            _context.Kunde.Add(kunde);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var byId = _context.Kunde.SingleOrDefault(x => x.Id == id);
+
+            if (byId == null)
+            {
+                return NotFound();
+            }
+
+            _context.Kunde.Remove(byId);
+            _context.SaveChanges();
+
+            return Ok("Objekt wurde gelöscht!");
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(Kunde kunde)
+        {
+            if (kunde.Id == 0)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _context.Kunde.Update(kunde);
+                _context.SaveChanges();
+                return Ok("Die Änderungen wurden gespeichert.");
+            }
+            catch(Exception ex) 
+            { 
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ Lagerverwaltung/Filters/ApiKeyAuthorization.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lagerverwaltung.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthorization : Attribute, IAsyncActionFilter
+    {        
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if(context.HttpContext.Request.Headers.TryGetValue("ApiKey", out var key))
+            {
+                var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+                var configApiKey = config.GetValue<string>("ApiKey");
+
+                if (key.Equals(configApiKey) == false)
+                {
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
+            }
+            else
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
